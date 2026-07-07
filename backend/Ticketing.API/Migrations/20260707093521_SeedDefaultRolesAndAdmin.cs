c946796d8df1a875ad779f5dae411a5e2d39fce0
Dan <uprise419@gmail.com>
2026-07-07 16:36:07 +0700

    melakukan update database

---
+++ backend/Ticketing.API/Migrations/20260707093521_SeedDefaultRolesAndAdmin.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Ticketing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDefaultRolesAndAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "Id", "CreatedAt", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1L, new DateTime(2026, 7, 7, 9, 35, 19, 142, DateTimeKind.Utc).AddTicks(7906), "SuperAdmin", new DateTime(2026, 7, 7, 9, 35, 19, 142, DateTimeKind.Utc).AddTicks(7911) },
+                    { 2L, new DateTime(2026, 7, 7, 9, 35, 19, 142, DateTimeKind.Utc).AddTicks(8866), "EventOrganizer", new DateTime(2026, 7, 7, 9, 35, 19, 142, DateTimeKind.Utc).AddTicks(8867) },
+                    { 3L, new DateTime(2026, 7, 7, 9, 35, 19, 142, DateTimeKind.Utc).AddTicks(8869), "Customer", new DateTime(2026, 7, 7, 9, 35, 19, 142, DateTimeKind.Utc).AddTicks(8870) }
+                });
+
+            migrationBuilder.InsertData(
+                table: "users",
+                columns: new[] { "Id", "CreatedAt", "DeletedAt", "Email", "IsActive", "Name", "Password", "Phone", "UpdatedAt" },
+                values: new object[] { 1L, new DateTime(2026, 7, 7, 9, 35, 19, 354, DateTimeKind.Utc).AddTicks(9893), null, "superadmin@ticket.com", true, "Super Admin Utama", "$2a$11$KAq9H9Xv3UcftDQ4s5nnr.7wWPgchWZPB5PrixTifw1yFzdJEA2ui", "081111111111", new DateTime(2026, 7, 7, 9, 35, 19, 354, DateTimeKind.Utc).AddTicks(9897) });
+
+            migrationBuilder.InsertData(
+                table: "user_roles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { 1L, 1L });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "user_roles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { 1L, 1L });
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "users",
+                keyColumn: "Id",
+                keyValue: 1L);
+        }
+    }
+}
--- backend/Ticketing.API/obj/Debug/net10.0/Ticketing.API.AssemblyInfo.cs
+++ backend/Ticketing.API/obj/Debug/net10.0/Ticketing.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ticketing.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa8f928b74240650cc10004338827c072086fe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfedd9ecaaa96d59fcfa4884266ddc582fb9d6d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ticketing.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ticketing.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
